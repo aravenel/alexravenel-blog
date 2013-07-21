e120ac51f2ea207b02848c7ac92db9d6a9ac25a9
Alex Ravenel <ravenel@gmail.com>
2013-07-21 17:10:25 -0400

    Updated to work with tuxlite theme

--- content/pages/contact.rst
+++ content/pages/contact.rst
@@ -4,4 +4,4 @@ Contact Alex
 :Author: Alex Ravenel
 :Date: 2012-01-25
 
-Contact alex on Twitter at @aravenel
+Contact alex on Twitter at @aravenel.
--- pelicanconf.py
+++ pelicanconf.py
@@ -16,18 +16,21 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+#LINKS =  (('Pelican', 'http://getpelican.com/'),
+          #('Python.org', 'http://python.org/'),
+          #('Jinja2', 'http://jinja.pocoo.org/'),
+          #('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('http://www.twitter.com/aravenel', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Twitter: @aravenel', 'http://www.twitter.com/aravenel'),)
+
+MENUITEMS = (
+    ('Home', SITEURL),
+)
 
 DEFAULT_PAGINATION = 5
 
-THEME = '/home/ravenel/code/pelican-themes/tuxlite_zf'
+THEME = 'themes/tuxlite_zf'
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True

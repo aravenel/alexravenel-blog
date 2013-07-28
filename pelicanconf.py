80eb4d04f4b0f2d0e775578a5209b9080b1218b2
Alex Ravenel <ravenel@gmail.com>
2013-07-28 13:30:39 -0400

    Changed the theme.

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,7 +11,7 @@ TIMEZONE = 'Europe/Paris'
 DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
+FEED_ALL_ATOM = 'feed.xml'
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
@@ -26,18 +26,18 @@ SOCIAL = (('twitter', 'http://www.twitter.com/aravenel'),
           ('github', 'https://github.com/aravenel'),
           ('linkedin', 'http://www.linkedin.com/profile/view?id=23712282'))
 
-MENUITEMS = (
-    ('Home', SITEURL),
-)
+#MENUITEMS = (
+    #('Home', SITEURL),
+#)
 
 DEFAULT_PAGINATION = 5
-SUMMARY_MAX_LENGTH = 20
+SUMMARY_MAX_LENGTH = 30
 DISPLAY_PAGES_ON_MENU = True
 DISPLAY_CATEGORIES_ON_MENU = False
 
 DISQUS_SITENAME = 'alexravenel'
 
-THEME = 'themes/svbhack/'
+THEME = 'themes/svbtle/'
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
